e380aa63eea05fe747820ecbe2c3e780edf2fdea
Gemer <gemer@wizmacau.com>
2015-11-16 17:57:12 +0800

    base struture

--- src/index.js
+++ src/index.js
@@ -2,33 +2,38 @@
 
 import React from 'react'
 import { render } from 'react-dom'
-import { compose, createStore, applyMiddleware } from 'redux'
+import { compose, createStore, applyMiddleware,combineReducers} from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import reducer from './reduces'
-import App from './containers/App'
-
+import App from 'containers/App'
 import { devTools, persistState } from 'redux-devtools';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+import DiffMonitor from 'redux-devtools-diff-monitor'
+
 
 const middleware = process.env.NODE_ENV === 'production' ? [thunk] : [thunk, logger()];
 
 const finalCreateStore = compose(
     applyMiddleware(...middleware),
-    devTools()
+    devTools(),
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
 )(createStore);
 
 const store = finalCreateStore(reducer);
-
+//if (module.hot) {
+//    module.hot.accept('./reduces', () =>
+//        store.replaceReducer(combineReducers(require('./reduces')))
+//    );
+//}
 render(
     <div>
         <Provider store={store}>
             <App />
         </Provider>
-        <DebugPanel top right bottom>
-            <DevTools store={store} monitor={LogMonitor}/>
-        </DebugPanel>
+
+        <DevTools store={store} monitor={DiffMonitor} shortcut='ctrl+d'/>
     </div>,
     document.getElementById('root')
 );
